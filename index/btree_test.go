1e8d8a9886fcbabc612b6fc0c5ae9bc648d13bea
SLOWDOWNO <shi13341455279@gmail.com>
2024-03-01 15:58:50 +0800

    feat: complete basic data reading and writing process

--- index/btree_test.go
+++ index/btree_test.go
@@ -11,26 +11,26 @@ func TestBtree_Put(t *testing.T) {
 	bt := NewBTree()
 
 	res1 := bt.Put(nil, &data.LogRecordPos{Fid: 1, Offset: 100})
-	assert.True(t, res1)
+	assert.Nil(t, res1)
 
 	res2 := bt.Put([]byte("a"), &data.LogRecordPos{Fid: 1, Offset: 2})
-	assert.True(t, res2)
+	assert.Nil(t, res2)
 }
 
 func TestBTree_Get(t *testing.T) {
 	bt := NewBTree()
 
 	res1 := bt.Put(nil, &data.LogRecordPos{Fid: 1, Offset: 100})
-	assert.True(t, res1)
+	assert.Nil(t, res1)
 
 	pos1 := bt.Get(nil)
 	assert.Equal(t, uint32(1), pos1.Fid)
 	assert.Equal(t, uint64(100), pos1.Offset)
 
 	res2 := bt.Put([]byte("a"), &data.LogRecordPos{Fid: 1, Offset: 2})
-	assert.True(t, res2)
+	assert.Nil(t, res2)
 	res3 := bt.Put([]byte("a"), &data.LogRecordPos{Fid: 1, Offset: 3})
-	assert.True(t, res3)
+	assert.Nil(t, res3)
 
 	pos2 := bt.Get([]byte("a"))
 	assert.Equal(t, uint32(1), pos2.Fid)
@@ -41,12 +41,12 @@ func TestBTree_Delete(t *testing.T) {
 	bt := NewBTree()
 
 	res1 := bt.Put(nil, &data.LogRecordPos{Fid: 1, Offset: 100})
-	assert.True(t, res1)
+	assert.Nil(t, res1)
 	res2 := bt.Delete(nil)
 	assert.True(t, res2)
 
 	res3 := bt.Put([]byte("a"), &data.LogRecordPos{Fid: 2, Offset: 888})
-	assert.True(t, res3)
+	assert.Nil(t, res3)
 	res4 := bt.Delete([]byte("a"))
 	assert.True(t, res4)
 }
